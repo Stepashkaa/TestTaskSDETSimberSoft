555e8db9ac6175557de4cf97d95b96d72b69c156
Stepan <afanasevstepan67@gmail.com>
2025-03-06 23:05:37 +0400

    init2

--- src/test/java/TestForm.java
+++ src/test/java/TestForm.java
@@ -16,26 +16,26 @@ public class TestForm {
     void init() {
         Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
         Configuration.browserSize = "1920x1080";
-        Configuration.browser = "edge";
-        Configuration.timeout = 1500000;
+        //Configuration.browser = "edge";
+        Configuration.timeout = 30000;
 
         open(Configuration.baseUrl);
         formPage = page(MainFormPage.class);
     }
 
-    @Test(description = "Проверка ввода имени")
+    @Test(description = "Проверка ввода имени", priority = 1)
     void testSetFirstNameField() {
-        formPage.setFirstName("Alex");
-    }
-    /*
-    @Test(description = "Проверка ввода фамилии", priority = 2, dataProvider = "testData")
-    void testSetLastNameField(String firstName, String lastName, String email, String number, String date, String month, String day, String subject, String photoPath, String address, String state, String city) {
-        formPage.setLastName(lastName);
+        formPage.setFirstName("Stepan");
     }
 
-    @Test(description = "Проверка ввода почты", priority = 3, dataProvider = "testData")
-    void testSetEmailField(String firstName, String lastName, String email, String number, String date, String month, String day, String subject, String photoPath, String address, String state, String city) {
-        formPage.setEmailField(email);
+    @Test(description = "Проверка ввода фамилии", priority = 2)
+    void testSetLastNameField() {
+        formPage.setLastName("Afanasev");
+    }
+
+    @Test(description = "Проверка ввода почты", priority = 3)
+    void testSetEmailField() {
+        formPage.setEmailField("stepan@yandex.ru");
     }
 
     @Test(priority = 4)
@@ -43,19 +43,19 @@ public class TestForm {
         formPage.clickOnRadioButtonGender();
     }
 
-    @Test(description = "Проверка ввода телефона", priority = 5, dataProvider = "testData")
-    void testSetNumberField(String firstName, String lastName, String email, String number, String date, String month, String day, String subject, String photoPath, String address, String state, String city) {
-        formPage.setNumber(number);
+    @Test(description = "Проверка ввода телефона", priority = 5)
+    void testSetNumberField() {
+        formPage.setNumber("9996667432");
     }
 
-    @Test(description = "Проверка выбора даты рождения", priority = 6, dataProvider = "testData")
-    void testSetDateField(String firstName, String lastName, String email, String number, String date, String month, String day, String subject, String photoPath, String address, String state, String city) {
-        formPage.setDate(date, month, day);
+    @Test(description = "Проверка выбора даты рождения", priority = 6)
+    void testSetDateField() {
+        formPage.setDate("2004", "October", "10");
     }
 
-    @Test(description = "Проверка ввода предмета", priority = 7, dataProvider = "testData")
-    void testSetSubjectField(String firstName, String lastName, String email, String number, String date, String month, String day, String subject, String photoPath, String address, String state, String city) {
-        formPage.setSubjects(subject);
+    @Test(description = "Проверка ввода предмета", priority = 7)
+    void testSetSubjectField() {
+        formPage.setSubjects("Information");
     }
 
     @Test(description = "Проверка работоспособности checkbox-а", priority = 8)
@@ -63,30 +63,30 @@ public class TestForm {
         formPage.clickCheckBox();
     }
 
-    @Test(description = "Провера загрузки фото", priority = 9, dataProvider = "testData")
-    void testSetPhoto(String firstName, String lastName, String email, String number, String date, String month, String day, String subject, String photoPath, String address, String state, String city) {
-        File file = new File(photoPath);
+    @Test(description = "Провера загрузки фото", priority = 9)
+    void testSetPhoto() {
+        File file = new File("src/main/resources/javaPicture.jpg");
         formPage.setPhoto(file);
     }
 
-    @Test(description = "Проверка ввода адреса", priority = 10, dataProvider = "testData")
-    void testSetTextOnCurrentAddressField(String firstName, String lastName, String email, String number, String date, String month, String day, String subject, String photoPath, String address, String state, String city) {
-        formPage.setTextOnCurrentAddress(address);
+    @Test(description = "Проверка ввода адреса", priority = 10)
+    void testSetTextOnCurrentAddressField() {
+        formPage.setTextOnCurrentAddress("Промышленная, 10");
     }
 
-    @Test(description = "Проверка работоспособности selectState", priority = 11, dataProvider = "testData")
-    void testSelectState(String firstName, String lastName, String email, String number, String date, String month, String day, String subject, String photoPath, String address, String state, String city) {
-        formPage.selectState(state);
+    @Test(description = "Проверка работоспособности selectState", priority = 11)
+    void testSelectState() {
+        formPage.selectState("Haryana");
     }
 
-    @Test(description = "Проверка работоспособности selectCity", priority = 12, dataProvider = "testData")
-    void testSelectCity(String firstName, String lastName, String email, String number, String date, String month, String day, String subject, String photoPath, String address, String state, String city) {
-        formPage.selectCity(city);
+    @Test(description = "Проверка работоспособности selectCity", priority = 12)
+    void testSelectCity() {
+        formPage.selectCity("Karnal");
     }
 
     @Test(description = "Нажатие кнопки submit и проверка полей", priority = 13)
     void testClickSubmitButton() {
         formPage.clickSubmitButton();
         sleep(10000);
-    }*/
+    }
 }
